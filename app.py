cf842b74423fe42322288e2cdfe8a2a1f080b09f
wen527555 <wen527555@gmail.com>
2022-11-22 07:07:46 +0800

    taipei-day-trip

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import request
 from flask import render_template #幫助取出樣板檔案的內容，送到前端
 from flask import session
 from mysql.connector import pooling
+from flask import jsonify 
+import math
 from mysql.connector import Error
-from flask import jsonify #使用jsonify模块来让网页直接显示json数据
 
 connection_pool=pooling.MySQLConnectionPool(
                                             pool_name="mysqlpool",
@@ -36,6 +37,114 @@ def thankyou():
 def error():
     massage=request.args.get("massage","")
     return render_template("error.html",massage=massage)
+
+#取得景點資料
+@app.route("/api/attractions",methods=["GET"]) 
+def attractions():
+
+    try:
+        input_Page=request.args.get("page")
+        input_Page=int(input_Page)
+        count=int(input_Page)*12
+        input_keyword=request.args.get("keyword")
+        connection_object=connection_pool.get_connection()
+        mycursor = connection_object.cursor(dictionary=True)
+        mycursor.execute("SELECT COUNT(`id`) FROM `attractions` ")
+        total_data = mycursor.fetchone()
+        total_data= total_data["COUNT(`id`)"]
+        total_Page=math.floor(total_data/12)
+        # print(total_Page)
+        print(input_keyword)   
+        if input_keyword != None:
+
+            sql="SELECT * FROM attractions WHERE category=%s OR LOCATE( %s ,`name`) LIMIT %s,12"
+            val=(input_keyword,"%"+input_keyword+"%",count)
+            mycursor.execute(sql,val)
+            myresult = mycursor.fetchall()
+            # print(myresult)
+            result_all=[]
+            for result in myresult:
+                new_images=[]
+                images=result["images"].split(",")
+                for n in range(len(images)):
+                    images_https=images[n]
+                    new_images.append(images_https)
+                result_all.append(
+                    {
+                        "id":result["id"],
+                        "name":result["name"],
+                        "category":result["category"],
+                        "description":result["description"],
+                        "address":result["address"],
+                        "transport":result["transport"],
+                        "lat":result["lat"],
+                        "lng":result["lng"],
+                        "images":new_images           
+                    })
+                if int(total_Page) > input_Page:        
+                    return jsonify({
+                        "nextPage":input_Page+1,
+                        "data": result_all
+                    })
+                elif int(total_Page) == input_Page:     
+                    return jsonify({
+                        "nextPage":"null",
+                        "data": result_all
+                    })
+                else:
+                    return jsonify({
+                        "error":True,
+                        "message":"無此頁面"
+                    })
+
+
+        else:
+            mycursor.execute("SELECT * FROM attractions ORDER BY id LIMIT %s,12 ", [count])  
+            myresult = mycursor.fetchall()
+            result_all=[]
+            for result in myresult:
+                new_images=[]
+                images=result["images"].split(",")
+                for n in range(len(images)):
+                    images_https=images[n]
+                    new_images.append(images_https)
+                result_all.append(
+                    {
+                        "id":result["id"],
+                        "name":result["name"],
+                        "category":result["category"],
+                        "description":result["description"],
+                        "address":result["address"],
+                        "transport":result["transport"],
+                        "lat":result["lat"],
+                        "lng":result["lng"],
+                        "images":new_images           
+                    })
+                if int(total_Page) > input_Page:        
+                    return jsonify({
+                        "nextPage":input_Page+1,
+                        "data": result_all
+                    })
+                elif int(total_Page) == input_Page:
+                    return jsonify({
+                        "nextPage":"null",
+                        "data": result_all
+                    })
+                else:
+                    return jsonify({
+                        "error":True,
+                        "message":"無此頁面"
+                    })
+    except:
+        return jsonify ({
+                "error":True,
+                "message":"伺服器內部錯誤"
+            })
+    finally:    
+        mycursor.close()
+        connection_object.close()
+        print("DONE") 
+
 
 #根據景點編號取得景點資料
 @app.route("/api/attraction/<attractionId>",methods=["GET"]) 
