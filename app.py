677ce6e510716f63981106375f7c5065e791c92b
wen527555 <wen527555@gmail.com>
2022-11-23 09:39:23 +0800

    ‘final’

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def error():
 def attractions():
 
     try:
-        input_Page=request.args.get("page")
+        input_Page=request.args.get("page") 
         input_Page=int(input_Page)
-        count=int(input_Page)*12
+        next_Page=int(input_Page)+1
         input_keyword=request.args.get("keyword")
         connection_object=connection_pool.get_connection()
         mycursor = connection_object.cursor(dictionary=True)
@@ -53,14 +53,14 @@ def attractions():
         total_data = mycursor.fetchone()
         total_data= total_data["COUNT(`id`)"]
         total_Page=math.floor(total_data/12)
+        if total_Page == input_Page:
+            next_Page = None
         # print(total_Page)
-        print(input_keyword)   
         if input_keyword != None:
-
-            sql="SELECT * FROM attractions WHERE category=%s OR LOCATE( %s ,`name`) LIMIT %s,12"
-            val=(input_keyword,"%"+input_keyword+"%",count)
-            mycursor.execute(sql,val)
+            mycursor.execute("SELECT * FROM attractions WHERE category=%s or LOCATE( %s ,`name`)  LIMIT %s,12 ", [input_keyword,input_keyword,(input_Page*12)])
             myresult = mycursor.fetchall()
+            if len(myresult)<12:
+                next_Page = None
             # print(myresult)
             result_all=[]
             for result in myresult:
@@ -81,25 +81,20 @@ def attractions():
                         "lng":result["lng"],
                         "images":new_images           
                     })
-                if int(total_Page) > input_Page:        
-                    return jsonify({
-                        "nextPage":input_Page+1,
+                if total_Page >= input_Page:        
+                    result ={
+                        "nextPage":next_Page,
                         "data": result_all
-                    })
-                elif int(total_Page) == input_Page:     
-                    return jsonify({
-                        "nextPage":"null",
-                        "data": result_all
-                    })
+                    } 
+                    ans=jsonify(result)
                 else:
-                    return jsonify({
+                    error ={
                         "error":True,
                         "message":"無此頁面"
-                    })
-
-
+                    }
+                    ans=jsonify(error)
         else:
-            mycursor.execute("SELECT * FROM attractions ORDER BY id LIMIT %s,12 ", [count])  
+            mycursor.execute("SELECT * FROM attractions ORDER BY id LIMIT %s,12 ", [(input_Page*12)])  
             myresult = mycursor.fetchall()
             result_all=[]
             for result in myresult:
@@ -120,22 +115,20 @@ def attractions():
                         "lng":result["lng"],
                         "images":new_images           
                     })
-                if int(total_Page) > input_Page:        
-                    return jsonify({
-                        "nextPage":input_Page+1,
+                if total_Page >= input_Page:        
+                    result ={
+                        "nextPage":next_Page,
                         "data": result_all
-                    })
-                elif int(total_Page) == input_Page:
-                    return jsonify({
-                        "nextPage":"null",
-                        "data": result_all
-                    })
+                    } 
+                    ans=jsonify(result)
                 else:
-                    return jsonify({
+                    error ={
                         "error":True,
                         "message":"無此頁面"
-                    })
-    except:
+                    }
+                    ans=jsonify(error)
+        return ans
+    except :
         return jsonify ({
                 "error":True,
                 "message":"伺服器內部錯誤"
@@ -146,6 +139,85 @@ def attractions():
         print("DONE") 
 
 
+#根據景點編號取得景點資料
+@app.route("/api/attraction/<attractionId>",methods=["GET"]) 
+def getattraction(attractionId):
+    try:
+        connection_object=connection_pool.get_connection()
+        mycursor = connection_object.cursor(dictionary=True)
+        mycursor.execute("SELECT COUNT(`id`) FROM `attractions` ")
+        nums = mycursor.fetchone()
+        nums= nums["COUNT(`id`)"]
+        if int(nums)<int(attractionId):
+            return  jsonify ({
+                "error":True,
+                "message":"景點編號不正確"
+            }) 
+        if int(nums)>=int(attractionId):
+            mycursor.execute("SELECT * FROM attractions WHERE  id=%s", [attractionId])
+            myresult=mycursor.fetchone()
+            result_all=[]
+            new_images=[]
+            images=myresult["images"].split(",")
+            # if '' in images:
+            # images.remove('')
+            for n in range(len(images)):
+                images_https=images[n]
+                new_images.append(images_https)
+
+            result_all=(
+                {
+                    "id":myresult["id"],
+                    "name":myresult["name"],
+                    "category":myresult["category"],
+                    "description":myresult["description"],
+                    "address":myresult["address"],
+                    "transport":myresult["transport"],
+                    "lat":myresult["lat"],
+                    "lng":myresult["lng"],
+                    "images":new_images
+                        
+                    })
+            return jsonify({
+                "data": result_all
+            })
+    except :
+        return  jsonify ({
+                "error":True,
+                "message":"伺服器內部錯誤"
+            })
+    finally:   
+        mycursor.close()
+        connection_object.close()
+        print("DONE") 
+
+#旅遊景點分類
+@app.route("/api/categories",methods=["GET"]) 
+def categories():
+    try:
+        connection_object=connection_pool.get_connection()
+        mycursor = connection_object.cursor(dictionary=True)
+        mycursor.execute("SELECT distinct category FROM `attractions` ")
+        myresult = mycursor.fetchall()
+        print(myresult)
+        categories=[]
+
+        for x in myresult:
+            n = x['category']
+            categories.append(n)
+        return jsonify({
+            "data":categories
+            })
+
+    except :
+        return  jsonify ({
+                "error":True,
+                "message":"伺服器內部錯誤"
+            })
+    finally:   
+        mycursor.close()
+        connection_object.close()
+        print("DONE") 
 
 
 app.run(host='0.0.0.0',port=3000,debug=True)
